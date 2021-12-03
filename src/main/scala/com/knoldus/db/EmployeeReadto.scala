adaf5db09abcfa3352c5dd901ed77ea238d71f21
Utkarsh Ambastha <utkarsh.ambastha@knoldus.com>
2021-12-03 20:15:34 +0530

    Made module for 3 questions and implemented TDD

---
+++ src/main/scala/com/knoldus/db/EmployeeReadto.scala
@@ -0,0 +1,9 @@
+package com.knoldus.db
+
+import com.knoldus.models.Employee
+
+class EmployeeReadto {
+
+  val employee:Employee = Employee("Utkarsh", "Ambastha", 22, 15000, "Intern", "Knoldus", "utkarsh.ambastha@knoldus.com")
+
+}
